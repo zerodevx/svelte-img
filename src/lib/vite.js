1fd1198d306ab59b4e28338cad0c13603aa6b97a
Jason Lee <jason@zerodevx.com>
2022-09-03 15:37:30 +0800

    Refactor `imagetools` config resolver

--- src/lib/vite.js
+++ src/lib/vite.js
@@ -6,35 +6,33 @@ function lqip(cfg, ctx) {
     const r = resize({ width: cfg.lqip }, ctx)
     const f = format({ format: 'webp', quality: '20' }, ctx)
     return async function (image) {
-      image = f(r(image))
-      const buffer = await image.toBuffer()
+      const buffer = await f(r(image)).toBuffer()
       setMetadata(image, 'base64', `data:image/webp;base64,${buffer.toString('base64')}`)
       return image
     }
   }
 }
 
-function set(url, defaults = 'width=480;1024;1920&format=avif;webp;jpg') {
-  const merge = new URLSearchParams(defaults)
-  for (const [key, val] of url.searchParams.entries()) merge.set(key, val)
-  return merge
-}
-
 function main(overrides = {}) {
   return imagetools({
-    defaultDirectives: (url) => set(url),
+    defaultDirectives: () => new URLSearchParams('width=480;1024;1920&format=avif;webp;jpg'),
     extendTransforms: (builtins) => [...builtins, lqip],
     extendOutputFormats: (builtinOutputFormats) => ({
       ...builtinOutputFormats,
       run: () => metadataFormat(['format', 'src', 'width', 'base64'])
     }),
     resolveConfigs: (e, f) => {
-      const idx = e.findIndex((i) => i[0] === 'lqip')
-      const lqip = idx > -1 ? parseInt(e.splice(idx, 1)[0][1][0]) : 16
-      return [...resolveConfigs(e, f), { lqip }]
+      if (e.findIndex((i) => i[0] === 'run') > -1) {
+        const idx = e.findIndex((i) => i[0] === 'lqip')
+        const lqip = idx > -1 ? parseInt(e.splice(idx, 1)[0][1][0]) : 16
+        const merge = new Map()
+        for (const [key, val] of e) merge.set(key, val)
+        return [...resolveConfigs([...merge], f), { lqip }]
+      }
+      return resolveConfigs(e, f)
     },
     ...overrides
   })
 }
 
-export { main as imagetools, set, lqip }
+export { main as imagetools, lqip }
