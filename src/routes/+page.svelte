ac8eba16cbe889005d08b1df3326205086f8209b
Jason Lee <jason@zerodevx.com>
2022-10-16 18:36:56 +0800

    Update deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,7 +24,7 @@ let selected = 0
   <Img class="w-full h-screen object-cover" src={cat01} alt="cat" loading="eager" />
   <div class="absolute inset-0 flex flex-col items-center justify-center prose-2xl max-w-none px-2">
     <h1>svelte-img</h1>
-    <p>Elegant responsive images for SvelteKit.</p>
+    <p>High-performance responsive images for SvelteKit.</p>
     <a
       class="h-14 flex items-center justify-center rounded-full text-xs font-bold tracking-wider bg-blue-600 hover:bg-blue-800 px-6 mt-8 mb-6"
       href="https://github.com/zerodevx/svelte-img">VISIT GITHUB REPO</a
@@ -32,66 +32,86 @@ let selected = 0
   </div>
 </div>
 
-<div class="max-w-7xl mx-auto prose px-4 mb-96">
+<div class="max-w-3xl mx-auto prose px-4">
   <h1>svelte-img</h1>
   <blockquote>
     Render the bare minimum, minimally invasive, LQIP-included HTML code to represent responsive
     images, served in multiple widths and next-gen formats.
   </blockquote>
-
   <ol>
-    <li class="mb-12">
+    <li>
       <p>
         Easily render a responsive image in 10 variants - <code>avif/webp/jpg</code> formats at
         <code>480/1024/1920</code> widths, with a low-quality image placeholder (LQIP) background.
       </p>
-      <Img class="w-full h-128 object-cover" src={cat02} alt="cat" />
     </li>
+  </ol>
+</div>
+<div class="max-w-7xl mx-auto px-4 mb-12">
+  <Img class="w-full h-128 object-cover" src={cat02} alt="cat" />
+</div>
 
-    <li class="mb-12">
+<div class="max-w-3xl mx-auto prose px-4">
+  <ol start="2">
+    <li>
       <p>
         Or reveal images with fade-in special effects. Delegate the heavy lifting to native browser
         handlers and existing build pipelines. The <code>svelte-img</code>
         component is intended to be a drop-in replacement of the native <code>&lt;img&gt;</code> tag
         and tries to work as equivalently as possible.
       </p>
-      <FxReveal class="w-full h-128 object-cover" src={cat03} alt="cat" />
     </li>
+  </ol>
+</div>
+<div class="max-w-7xl mx-auto px-4 mb-12">
+  <FxReveal class="w-full h-128 object-cover" src={cat03} alt="cat" />
+</div>
 
-    <li class="mb-12">
+<div class="max-w-3xl mx-auto prose px-4">
+  <ol start="3">
+    <li>
       <p>
         Or add parallax special effects to images. All images should still work without Javascript.
         Try disabling JS and refreshing the page. Also check out the lighthouse score.
       </p>
-      <FxParallax class="w-full h-128" src={cat04} alt="cat" />
     </li>
+  </ol>
+</div>
+<div class="max-w-7xl mx-auto px-4 mb-12">
+  <FxParallax class="w-full h-128" src={cat04} alt="cat" />
+</div>
 
+<div class="max-w-3xl mx-auto prose px-4">
+  <ol start="4">
     <li>
       <p>
         Image <code>src</code> can be dynamically updated - well, this requires Javascript because
         <em>interactivity</em>.
       </p>
-      <div class="flex flex-col md:flex-row">
-        <div class="flex justify-center">
-          <div class="w-96 h-96 md:w-128 md:h-128">
-            <Img class="w-full h-full not-prose" src={dynamic[selected]} alt="cat" />
-          </div>
-        </div>
-        <div class="flex flex-wrap justify-center mt-4 md:flex-1 md:mt-6">
-          {#each dynamic as src, i}
-            <div class="w-32 h-32 md:w-36 md:h-36 m-2">
-              <Img
-                class="w-full h-full border-4 cursor-pointer {selected === i
-                  ? 'border-red-500'
-                  : 'border-gray-100'}"
-                {src}
-                alt="cat"
-                on:click={() => (selected = i)}
-              />
-            </div>
-          {/each}
-        </div>
-      </div>
     </li>
   </ol>
+</div>
+
+<div class="max-w-7xl mx-auto px-4 mb-96">
+  <div class="flex flex-col md:flex-row">
+    <div class="flex justify-center">
+      <div class="w-96 h-96 md:w-128 md:h-128">
+        <Img class="w-full h-full not-prose" src={dynamic[selected]} alt="cat" />
+      </div>
+    </div>
+    <div class="flex flex-wrap justify-center mt-4 md:flex-1 md:mt-6">
+      {#each dynamic as src, i}
+        <div class="w-32 h-32 md:w-36 md:h-36 m-2">
+          <Img
+            class="w-full h-full border-4 cursor-pointer {selected === i
+              ? 'border-red-500'
+              : 'border-gray-100'}"
+            {src}
+            alt="cat"
+            on:click={() => (selected = i)}
+          />
+        </div>
+      {/each}
+    </div>
+  </div>
 </div>
