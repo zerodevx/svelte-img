fd20736fa4074fdcdc414c0ad51c32cc29d2dfef
Jason Lee <jason@zerodevx.com>
2023-01-27 20:30:35 +0800

    Add `imagetools` tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,8 +1,13 @@
+import detect from 'is-port-reachable'
+
+const reuseExistingServer = await detect(5173, { host: 'localhost' })
+
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
   webServer: {
-    command: 'npm run build && npm run preview',
-    port: 4173
+    command: 'npm run dev',
+    port: 5173,
+    reuseExistingServer
   },
   testDir: 'tests'
 }
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+import testBasic from '../cat01.jpg?run'
+import testNoLqip from '../cat01.jpg?lqip=0&run'
+import testWidthOverride from '../cat01.jpg?width=480;1024&run'
+
+const s = (obj = {}) => JSON.stringify(obj)
+
+const tests = [
+  { id: 'basic', name: 'default variants are generated', view: s(testBasic) },
+  { id: 'nolqip', name: 'no lqip', view: s(testNoLqip) },
+  { id: 'width', name: 'width override', view: s(testWidthOverride) }
+]
+</script>
+
+<svelte:head>
+  <meta name="robots" content="noindex" />
+</svelte:head>
+
+<div class="prose container mx-auto">
+  <h1>Tests</h1>
+  <section>
+    <h2>Imagetools</h2>
+    <ol>
+      {#each tests as { id, name, view }}
+        <li>
+          {name}
+          <pre><code data-testid={id} class="whitespace-normal">{@html view}</code></pre>
+        </li>
+      {/each}
+    </ol>
+  </section>
+</div>
