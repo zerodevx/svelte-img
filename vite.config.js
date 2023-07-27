586f6382ca2947ad76244573d3cf88814f6f252d
Jason Lee <jason@zerodevx.com>
2023-07-27 22:57:14 +0700

    Update tooling

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { defineConfig } from 'vite'
 import { imagetools } from './src/lib/vite'
 
-/** @type {import('vite').UserConfig} */
-const config = {
+export default defineConfig({
   plugins: [sveltekit(), imagetools()]
-}
-
-export default config
+})
