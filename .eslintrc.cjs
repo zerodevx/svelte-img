0e3610a54e95fbc75f434381fb71374e53717a58
Jason Lee <jason@zerodevx.com>
2023-07-27 22:57:14 +0700

    Update tooling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,15 @@
 module.exports = {
   root: true,
-  extends: ['eslint:recommended', 'prettier'],
-  plugins: ['svelte3'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+  extends: ['eslint:recommended', 'plugin:svelte/recommended', 'prettier'],
   parserOptions: {
     sourceType: 'module',
-    ecmaVersion: 2022
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte']
   },
   env: {
     browser: true,
     es2017: true,
     node: true
   },
-  rules: {
-    'no-tabs': 'error',
-    'no-unexpected-multiline': 'error'
-  }
+  rules: { 'no-tabs': 'error', 'no-unexpected-multiline': 'error' }
 }
--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
-import preprocess from 'svelte-preprocess'
+import { vitePreprocess } from '@sveltejs/kit/vite'
 import adapter from '@sveltejs/adapter-static'
+import { readFileSync } from 'node:fs'
 
+const { version: name } = JSON.parse(readFileSync(new URL('package.json', import.meta.url), 'utf8'))
 const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,13 +13,10 @@ const config = {
     }),
     paths: {
       base: dev ? '' : '/svelte-img'
-    }
+    },
+    version: { name }
   },
-  preprocess: [
-    preprocess({
-      postcss: true
-    })
-  ]
+  preprocess: [vitePreprocess({})]
 }
 
 export default config
