6d10186c4d0afb32006dc47eac1f220d4fdb248b
Jason Lee <jason@zerodevx.com>
2023-08-10 16:27:39 +0800

    Update tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,12 @@
-import detect from 'is-port-reachable'
-
-const reuseExistingServer = await detect(5173, { host: 'localhost' })
-
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
 const config = {
+  testDir: 'tests',
+  testMatch: '**/*.js',
   webServer: {
     command: 'npm run dev',
     port: 5173,
-    reuseExistingServer
-  },
-  testDir: 'tests'
+    reuseExistingServer: true
+  }
 }
 
 export default config
