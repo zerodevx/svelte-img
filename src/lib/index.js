8c4ee22877868179fe477a52e851a8eeeb780757
Luca Sartori <luca@ohmybugs.com>
2023-08-17 16:50:52 +0200

    Re-export instead of import and then export to optimize tree shaking

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,4 @@
-import Img from './SvelteImg.svelte'
-import FxReveal from './FxReveal.svelte'
-import FxParallax from './FxParallax.svelte'
-import { observe } from './utils.js'
-
-export default Img
-export { FxReveal, FxParallax, observe }
+export { default } from './SvelteImg.svelte'
+export { default as FxReveal } from './FxReveal.svelte'
+export { default as FxParallax } from './FxParallax.svelte'
+export { observe } from './utils.js'
