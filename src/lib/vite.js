9f641a8e7c42f81bc6dca5c376b352bdbeba38cf
Jason Lee <jason@zerodevx.com>
2023-08-26 16:19:55 +0800

    Clone image before running lqip op

--- src/lib/vite.js
+++ src/lib/vite.js
@@ -6,16 +6,18 @@ function run(cfg) {
     const lqip = (cfg && parseInt(cfg)) ?? 16
     if (lqip) {
       const { image } = metadatas.find((i) => i.src === pic.img.src)
-      let data
       if (lqip > 1) {
-        const buf = await image.resize({ width: lqip }).toFormat('webp', { quality: 20 }).toBuffer()
-        data = buf.toString('base64')
+        const buf = await image
+          .clone()
+          .resize({ width: lqip })
+          .toFormat('webp', { quality: 20 })
+          .toBuffer()
+        pic.img.lqip = buf.toString('base64')
       } else {
         const { dominant } = await image.stats()
         const { r, g, b } = dominant
-        data = '#' + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)
+        pic.img.lqip = '#' + ((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)
       }
-      pic.img.lqip = data
     }
     return pic
   }
