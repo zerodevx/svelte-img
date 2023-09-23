f45e4e3cceae077c4aa51abb17c1864fc4f6a312
Jason Lee <jason@zerodevx.com>
2023-09-23 21:39:49 +0800

    Add test for profile feat

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,12 @@ import { defineConfig } from 'vite'
 import { imagetools } from './src/lib/vite.js'
 
 export default defineConfig({
-  plugins: [sveltekit(), imagetools()]
+  plugins: [
+    sveltekit(),
+    imagetools({
+      profiles: {
+        run2: new URLSearchParams('w=32&h=16&fit=cover&format=jpg')
+      }
+    })
+  ]
 })
