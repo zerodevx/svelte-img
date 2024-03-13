d5e2ce9d990ddb895c98abdd355cbc52ab0547ac
Joel Kuzmarski <leoj3n@gmail.com>
2024-03-13 05:56:27 -0500

    Upgrade packages

--- src/lib/Picture.svelte
+++ src/lib/Picture.svelte
@@ -7,14 +7,7 @@ export let sizes = undefined
 let srcs = []
 
 $: if (len(sources)) {
-  const list = []
-  for (const [format, imgs] of Object.entries(sources)) {
-    list.push({
-      format,
-      srcset: imgs.map((i) => `${i.src} ${i.w}w`).join()
-    })
-  }
-  srcs = list
+  srcs = Object.entries(sources)
 } else {
   srcs = []
 }
@@ -22,7 +15,7 @@ $: if (len(sources)) {
 
 {#if len(srcs)}
   <picture>
-    {#each srcs as { format, srcset }}
+    {#each srcs as [format, srcset]}
       <source type="image/{format}" {sizes} {srcset} />
     {/each}
     <slot />
