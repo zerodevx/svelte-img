7051993dacc3e69d5496325e2a2ba34e5e7f1e1b
Nagy Botond <75550932+westernwontons@users.noreply.github.com>
2024-05-11 15:33:24 +0300

    Formatting

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static'
 import { readFileSync } from 'node:fs'
 
