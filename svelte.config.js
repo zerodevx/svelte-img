ec9bb0eb0e3048494ce0d4d3af3097c3d9aeaceb
Nagy Botond <75550932+westernwontons@users.noreply.github.com>
2024-05-11 12:52:14 +0300

    Update @sveltejs/vite-plugin-svelte to latest

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import { vitePreprocess } from '@sveltejs/kit/vite'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-static'
 import { readFileSync } from 'node:fs'
 
